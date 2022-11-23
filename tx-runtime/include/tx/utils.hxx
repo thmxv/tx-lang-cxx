541630c0ccb7554b26b17ba510ca50fc5c1cdf1f
Xavier Thomas <xavier.thomas.1980@gmail.com>
2022-11-23 02:38:48 -0300

    Add power_of_2_ceil utility

--- tx-runtime/include/tx/utils.hxx
+++ tx-runtime/include/tx/utils.hxx
@@ -32,7 +32,7 @@ template <typename Enum>
     return static_cast<std::underlying_type_t<Enum>>(enumeration);
 }
 
-[[noreturn]] inline void exit(ExitCode code) {
+[[noreturn]] inline void exit(ExitCode code) noexcept {
     // NOLINTNEXTLINE(concurrency-mt-unsafe)
     std::exit(to_underlying(code));
 }
@@ -40,7 +40,7 @@ template <typename Enum>
 [[noreturn]] inline void report_and_abort(
     std::string_view message,
     const source_location location = source_location::current()
-) {
+) noexcept {
     fmt::print(
         stderr,
         FMT_STRING("[{:s}:{:d}:{:d}] Error in {:s}(): {}\n"),
@@ -55,17 +55,29 @@ template <typename Enum>
 
 [[noreturn]] inline void unreachable(
     const source_location location = source_location::current()
-) {
+) noexcept {
     if constexpr (IS_DEBUG_BUILD) {
         report_and_abort("This code should have been unreachable", location);
     }
     __builtin_unreachable();
 }
 
-[[nodiscard]] inline constexpr bool has_integer_value(float_t val) {
+[[nodiscard]] inline constexpr bool has_integer_value(float_t val) noexcept {
     float_t int_part = 0.0;
     const auto fract_part = std::modf(val, &int_part);
     return fract_part == 0.0 && !std::isinf(int_part);
+}
+
+// From:
+// http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
+[[nodiscard]] inline constexpr i32 power_of_2_ceil(i32 n) noexcept {
+    n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    return ++n;
 }
 
 }  // namespace tx
